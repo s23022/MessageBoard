dd3b0c93eea6f6a786ae3f562b25fb43483ad7b5
s23022 <s23022@std.it-college.ac.jp>
2024-06-28 13:15:01 +0900

    ログイン機能を書いてる途中

--- src/main/kotlin/ip/ac/it_college/std/s23022/messege/board/domain/model/User.kt
+++ src/main/kotlin/ip/ac/it_college/std/s23022/messege/board/domain/model/User.kt
@@ -2,6 +2,7 @@ package ip.ac.it_college.std.s23022.messege.board.domain.model
 
 import ip.ac.it_college.std.s23022.messege.board.domain.types.RoleType
 
+
 data class User(
     val id: Long,
     val name: String,
